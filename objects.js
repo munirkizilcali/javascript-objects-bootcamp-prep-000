4c2f2669afbe32a6bf38b9d6a704285f6859794e
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-15 17:44:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,8 +5,8 @@ function updatePlaylist(obj, aName, sTitle) {
   return Object.assign({}, obj, {[aName]:sTitle})
 }
 function removeFromPlaylist(obj, aName) {
-  delete obj.[aName]
-  return obj
+  return obj.[aName]
+  //return obj
 }
 
 console.log(removeFromPlaylist(playlist,`artistName`))
