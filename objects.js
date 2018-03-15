36f5ee602894c01718abe62c816a3da405b1e8cb
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-15 17:37:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,6 @@
 var playlist = {
   artistName: `songtitle`
 }
-
 function updatePlaylist(obj, aName, sTitle) {
   return Object.assign({}, obj, {[aName]:sTitle})
 }
-console.log(updatePlaylist(playlist, `Vangelis`, `Voices`))
